a287a788a85b98b5cae5e724b24ccd960eb5fe05
Patrick Simmons <psimmons@pensando.io>
2025-09-18 03:12:03 -0500

    Works on simple testcase

--- llvm/lib/Target/AMDGPU/AMDGPU.h
+++ llvm/lib/Target/AMDGPU/AMDGPU.h
@@ -268,6 +268,9 @@ extern char &AMDGPUPreloadKernelArgumentsLegacyID;
 void initializeAMDGPUNextUseAnalysisWrapperPass(PassRegistry &);
 extern char &AMDGPUNextUseAnalysisID;
 
+void initializeSIRestoreNormalEpilogLegacyPass(PassRegistry &);
+extern char &SIRestoreNormalEpilogLegacyID;
+
 // Passes common to R600 and SI
 FunctionPass *createAMDGPUPromoteAlloca();
 void initializeAMDGPUPromoteAllocaPass(PassRegistry&);
